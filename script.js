a02d2183e541a2c14a106628f949fab4e50ccf27
rehmatkhan619 <rehmatkhanbts@gmail.com>
2024-01-31 21:27:48 -0800

    added

---
+++ script.js
@@ -0,0 +1,24 @@
+      //   ||||| STACK AND HEAP||||
+
+      //STACK is for premative (string ,numbr and 6 other).This will give the copy 
+      //HEAP is used in non premative (object,string and function ) this will give you orgginal value
+       
+      // EXAMPLE of stack   ......
+      let myName   ="Rehmat ullah";
+      let myAnotherName  =myName;
+      myAnotherName   ="khan"
+
+      console.log(myAnotherName)//khan   
+      console.log(myName)// Rehmat ullah
+
+      ///EXAMPLE OF HEAP.....
+      let userOne={
+        name:  "jhone",
+        email : "jhone@gmail.com"
+      };
+      let userTwo=userOne;
+      userTwo.email="smith@gmail.com"
+      console.log(userOne.email);  // smith@gmail.com
+      console.log(userTwo.email);  //smith@gmail.com
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
